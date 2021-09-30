5bd0f9b68f3bfbb606e1c5f77b39e43230607d76
Taku Iwahashi <taku.stylelab@gmail.com>
2021-09-30 16:04:15 +0900

    add - 下書きプレビューコンポーネント

---
+++ pages/draft/news/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="main">
+    <h1 class="title">{{ title }}</h1>
+    <p class="publishedAt">{{ publishedAt }}</p>
+  </main>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      data: {}
+    };
+  },
+  async created() {
+    const query = this.$route.query;
+    if (query.id === undefined || query.draftKey === undefined) {
+      return;
+    }
+    const { data } = await axios.get(
+      `https://stylelab-jamstacktest.microcms.io/api/v1/news/${query.id}?draftKey=${query.draftKey}`,
+      {
+        headers: { "X-API-KEY": "e0c83144-fb8b-4ddc-84cd-e0ecd86a7337" }
+      }
+    );
+    this.data = data;
+  }
+};
+</script>
